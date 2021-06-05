55634a4434202e3f65b510271dd3554455f2ba55
jojo2829 <joulinli@gmail.com>
2021-06-05 21:11:24 +0000

    complete

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ class Account {
 
   constructor(username) {
     this.username = username;
-    this.balance = 0;
+    this.transactions = [];
   }
 
+  get balance() {
+    let balance = 0;
+    for (let t of this.transactions) {
+      balance += t.value;
+    }
+    return balance;
+  }
+
+  addTransaction(trans) {
+    this.transactions.push(trans);
+  }
 }
 
 class Transaction {
@@ -15,10 +26,15 @@ class Transaction {
     this.amount  = amount;
     this.account = account;
 
+
   }
 
   commit() {
-    this.account.balance += this.value;
+    if (!this.isAllowed()) return false;
+    this.value;
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
   }
 
 }
@@ -28,30 +44,41 @@ class Deposit extends Transaction{
   get value() {
     return this.amount;
   }
+
+  isAllowed() {
+    return true;
+  }
 }
 
 class Withdrawal extends Transaction {
 
-
   get value() {
     return -this.amount;
   }
+
+  isAllowed() {
+    if (this.account.balance - this.amount >= 0) {
+      return true;
+    };
+    return false;
+  }
+
 }
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
-const myAccount = new Account("snow-patrol");
+const myAccount = new Account('billybob');
 
-t1 = new Withdrawal(50.25, myAccount);
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+//console.log("transaction 1: ", t1);
 
-t2 = new Withdrawal(9.99, myAccount);
+const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+//console.log("transaction 2: ", t2);
 
-t3 = new Deposit(120.00, myAccount);
-t3.commit();
-console.log('Transaction 3:', t3);
 
-console.log('Balance:', myAccount);
+console.log('Ending Balance:', myAccount.balance);
+console.log("account transaction", myAccount.transactions);
